e2ba7011b0436540f5e28d07f0322a930d1a0ec1
Jamey Kirk <jameykirk@gmail.com>
2021-05-23 17:06:46 +0900

    setting up event listeners

--- setup.js
+++ setup.js
@@ -1,1 +1,16 @@
 "use strict"
+
+
+/* Global Values */
+function test () {
+console.log(document.forms.setup.player_count.value);
+}
+
+/* Event Listeners */
+var submitBtn = document.forms.setup.elements.submit;
+submitBtn.addEventListener("click", function() {
+    test();
+
+})
+
+/* Session Storage Functions to Get and Set Data */
