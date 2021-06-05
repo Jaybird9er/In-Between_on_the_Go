b86f262f50f3d1d34e64b15d587b78db4899563b
Jamey Kirk <jameykirk@gmail.com>
2021-06-05 15:49:11 +0900

    starting form data object

--- setup.js
+++ setup.js
@@ -15,48 +15,60 @@ var setupForm = document.forms.setup;
 var playerCount = setupForm.elements.player_count;
 var chipCount = setupForm.elements.player_chip_count;
 var chipValue = setupForm.elements.chip_value;
-
 var buyIn = setupForm.elements.buy_in;
 var gamePurse = setupForm.elements.game_purse;
 var playStyle = setupForm.elements.play_style;
-
 var submitBtn = setupForm.submit;
 
 
 /* Event Listeners */
 window.addEventListener("load", function() {
 
+    // updates game settings
     playerCount.onchange = update;
     chipCount.onchange = update;
     chipValue.onchange = update;
     playStyle.onchange = update;
-    // calculate Player Buy-in
-    if (chipCount > 0)
-        buyIn = buyInCalc;
+
+    // setGame function creates creates object from form data
         
 });
 
+// displays game settings to user
 function update() {
     console.log(parseInt(chipCount.value));
     console.log(parseFloat(chipValue.value));
     console.log(parseInt(playerCount.value));
-    var cash = parseFloat(chipCount.value) * parseFloat(chipValue.value)
-    var prize = cash * playerCount.value;
-    //console.log(cash.toLocaleString('en-US', {style: "currency", currency: "USD"}));
-    buyIn.value = cash.toFixed(2);
-    gamePurse.value = prize.toFixed(2);
-    // var hands = setupForm.querySelector("input[name='play_style']:checked");
-    /* This doesn't seem to be working quite right */
+    var playerStakes = parseFloat(chipCount.value) * parseFloat(chipValue.value)
+    var purse = playerStakes * playerCount.value;
+    //console.log(playerStakes.toLocaleString('en-US', {style: "currency", currency: "USD"}));
+    buyIn.value = playerStakes.toFixed(2);
+    gamePurse.value = purse.toFixed(2);
     if (playStyle.value === "hand_limit") {
         setupForm.hand_count.hidden = false;
+        setupForm.hand_count.required = true;
+        document.getElementById("handL_description").hidden = false;
+        document.getElementById("consensus_description").hidden = true;
+        document.getElementById("oneW_description").hidden = true;
     }
-    else if (playStyle.value === "one_winner" || playStyle.value === "consensus") {
+    else if (playStyle.value === "consensus") {
         setupForm.hand_count.hidden = true;
-        setupForm.hand_count.value = "";
-
+        setupForm.hand_count.required = false;
+        setupForm.elements.hand_count.value = "";
+        document.getElementById("handL_description").hidden = true;
+        document.getElementById("consensus_description").hidden = false;
+        document.getElementById("oneW_description").hidden = true;
+    }
+    else if (playStyle.value === "one_winner") {
+        setupForm.hand_count.hidden = true;
+        setupForm.hand_count.required = false;
+        setupForm.elements.hand_count.value = "";
+        document.getElementById("handL_description").hidden = true;
+        document.getElementById("consensus_description").hidden = true;
+        document.getElementById("oneW_description").hidden = false;
     }
     console.log(playStyle.value);
 }
 
 
-/* Session Storage Functions to Get and Set Data */
+/* game form data object */
